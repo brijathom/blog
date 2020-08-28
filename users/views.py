3f837f944747e5ab628ea156a0eee271666a18e4
Brian Thomas <brianjamesthomas@live.com>
2020-08-28 15:16:10 +0100

    Add additional views and pages for users

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import RegistrationForm
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 
@@ -9,6 +10,11 @@ def register(request):
         form = RegistrationForm(request.POST)
         if form.is_valid():
             form.save()
+            username = request.POST['username']
+            password = request.POST['password1']
+            user = authenticate(request, username=username, password=password)
+            login(request, user)
+            return redirect('blog_home')
     else:
         form = RegistrationForm()
 
