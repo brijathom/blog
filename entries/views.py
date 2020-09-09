7e707087d8c43c88aaabc80ba990706c3b092410
Brian Thomas <brianjamesthomas@live.com>
2020-09-09 12:27:10 +0100

    Enable blog posts to be edited

--- entries/views.py
+++ entries/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from .models import Entry
 from .forms import EntryForm
 
@@ -21,8 +21,19 @@ class CreateEntryView(CreateView):
     model = Entry
     form_class = EntryForm
     template_name = 'entries/create_entry.html'
-    # fields = ['entry_title', 'entry_text']
 
     def form_valid(self, form):
         form.instance.entry_author = self.request.user
         return super().form_valid(form)
+
+
+class UpdateEntryView(UpdateView):
+    model = Entry
+    form_class = EntryForm
+    template_name = 'entries/update_entry.html'
+
+
+class DeleteEntryView(UpdateView):
+    model = Entry
+    form_class = EntryForm
+    template_name = 'entries/update_entry.html'
