36d76cfa586349c4e8c593a43dd6a8c81a0becaf
Brian Thomas <brianjamesthomas@live.com>
2021-03-28 18:53:15 +0100

    Update the model for the Entry class

--- entries/models.py
+++ entries/models.py
@@ -6,7 +6,6 @@ from ckeditor.fields import RichTextField
 class Entry(models.Model):
 
     entry_title = models.CharField(max_length=50)
-    # entry_text = models.TextField()
     entry_text = RichTextField(blank=True, null=True)
     entry_date = models.DateTimeField(auto_now_add=True)
     entry_author = models.ForeignKey(User, on_delete=models.CASCADE)
