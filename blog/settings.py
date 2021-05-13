a475b705c5c91bffb5ad475f55c4c2230f635961
Brian Thomas <brianjamesthomas@live.com>
2021-05-13 20:05:47 +0100

    Adjust the main title

--- blog/settings.py
+++ blog/settings.py
@@ -24,7 +24,6 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('BLOG_KEY')
 
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
